bc3d7569bb0a1f4d264ed20865628ddbf6641624
Matej <Perse>
2022-10-27 10:35:57 +0200

    Added all the required classes for notebook to init function

--- odc_sh/__init__.py
+++ odc_sh/__init__.py
@@ -1,7 +1,18 @@
 from .engine import Datacube
 
 from .commercial.sentinelsub_commercial_data import SentinelHubCommercialData
-from .commercial.commercial_data_base import CommercialSearchResponse
+from .commercial.commercial_data_base import (
+    CommercialSearchResponse, 
+    Providers, 
+    AirbusConstellation, 
+    ThumbnailType, 
+    ScopeType, 
+    ScopeBundle,
+    SkySatType, 
+    SkySatBundle,
+    WorldViewKernel, 
+    WorldViewSensor
+)
 
 
 __version__ = "1.0.0"
--- odc_sh/commercial/__init__.py
+++ odc_sh/commercial/__init__.py
@@ -1,2 +1,13 @@
 from .sentinelsub_commercial_data import SentinelHubCommercialData
-from .commercial_data_base import CommercialSearchResponse
+from .commercial_data_base import (
+    CommercialSearchResponse, 
+    Providers, 
+    AirbusConstellation, 
+    ThumbnailType, 
+    ScopeType, 
+    ScopeBundle,
+    SkySatType, 
+    SkySatBundle,
+    WorldViewKernel, 
+    WorldViewSensor
+)
